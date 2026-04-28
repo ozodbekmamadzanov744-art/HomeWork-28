18ac16cf78db0b67ba9625ec639d7bd3a9f8a91c
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-04-28 17:50:16 +0600

    Sorting by breed

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -7,5 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
 
+        var cats = Cat.makeCats(10);
+        Printer.print(cats);
+
+        cats.sort(Comparator.comparing(Cat::getBreed));
+        Printer.print(cats);
+
+
     }
 }
